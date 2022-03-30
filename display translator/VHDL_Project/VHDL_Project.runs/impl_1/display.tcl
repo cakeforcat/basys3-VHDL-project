b61cdcc018076da1cbabd6cd4fbf91718d4e47eb
cakeforcat <jakub.czarny.2020@uni.strath.ac.uk>
2022-03-30 15:45:40 +0100

    update sequence

--- display translator/VHDL_Project/VHDL_Project.runs/impl_1/display.tcl
+++ display translator/VHDL_Project/VHDL_Project.runs/impl_1/display.tcl
@@ -124,7 +124,6 @@ set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
   set_param chipscope.maxJobs 4
-  set_param xicom.use_bs_reader 1
   open_checkpoint display_routed.dcp
   set_property webtalk.parent_dir {C:/Users/cakef/Documents/GitHub/basys3-VHDL-project/display translator/VHDL_Project/VHDL_Project.cache/wt} [current_project]
 set_property TOP display [current_fileset]
